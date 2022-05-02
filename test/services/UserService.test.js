19e9af964bd671e18a413888a16861cacc84f668
mibarra24 <79391139+mibarra24@users.noreply.github.com>
2022-05-01 22:44:40 -0700

    new docs added

---
+++ test/services/UserService.test.js
@@ -0,0 +1,13 @@
+const UserService = require('./../../app/services/UserService')
+
+describe("Test for UserService", () => {
+
+    TextDecoderStream("1. Create a new user using the UserService", () =>{
+        const user = UserService.create(1,"paralink","Marco")
+        expect(user.username).toBe("paralink")
+        expect(user.name).toBe("Marco")
+        expect(user.id).toBe(1)
+        expect(user.bio).not.toBeUndefined()
+
+    })
+})
