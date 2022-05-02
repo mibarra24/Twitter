1f55090636c49d29a318b93075b0c3a50d8192d1
mibarra24 <79391139+mibarra24@users.noreply.github.com>
2022-05-01 21:13:58 -0700

    Added new test

--- app/models/User.js
+++ app/models/User.js
@@ -4,8 +4,23 @@ class User {
         this.username = username
         this.name = name
         this.bio = bio
-        this.dateCreated = dateCreated
-        this.lastUpdated = lastUpdated
+        this.dateCreated = new Date()
+        this.lastUpdated = new Date()
+    }
+    get getUsername(){
+        return this.username
+    }
+
+    get getBio(){
+        return this.bio
+    }
+
+    get getDateCreated(){
+        return this.dateCreated
+    }
+
+    get getLastUpdated(){
+        return this.lastUpdated
     }
 }
 
--- test/models/User.test.js
+++ test/models/User.test.js
@@ -4,7 +4,7 @@ describe("Unit Test for User class", () => {
 
     test('Create an User object', () => {
         //Aqui invocas el codigo que vas a usar en tu app
-        const user = new User(1, "paralink", "Marco", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "paralink", "Marco", "Bio")
 
         //Aqui validad los resultados de ese codigo
         //Esta es una comparacion que va a igualar el valor de la izquierda con el de la derecha
@@ -12,7 +12,15 @@ describe("Unit Test for User class", () => {
         expect(user.username).toBe("paralink")
         expect(user.name).toBe("Marco")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()//verifica que el valor no sea undefine
+        expect(user.lastUpdated).not.toBeUndefined()
+    })
+
+    test('Add getters',() => {
+        const user = new User(1,"paralink","Marco","Bio")
+        expect(user.getUsername).toBe("paralink")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
     })
 })
