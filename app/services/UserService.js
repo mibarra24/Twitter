87fcf31b6d75524a933f5a6fd80ead06d4babfe7
mibarra24 <79391139+mibarra24@users.noreply.github.com>
2022-05-01 23:31:33 -0700

    new info

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,6 +4,42 @@ class UserService {
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
+
+    static getInfo(user){
+        return Object.values(user)
+    }
+
+    static getInfo(name, username){
+        user.setBio = bio
+    }
+
+   static updateBio(user, bio){
+    user.setBio = bio
+  }
+
+  static updateUserUsername(user, username){
+    user.setUsername = username
+  }
+
+  static getAllUsernames(users){
+    const usersUsernames = users.map( user => user.username)
+    return usersUsernames
+  }
+
+  static updateBio(user, bio){
+    user.setBio = bio
+  }
+
+  static updateUserUsername(user, username){
+    user.setUsername = username
+  }
+
+  static getAllUsernames(users){
+    const usersUsernames = users.map( user => user.username)
+    return usersUsernames
+  }
+
+
 }
 
 module.exports = UserService
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -10,4 +10,29 @@ describe("Test for UserService", () => {
         expect(user.bio).not.toBeUndefined()
 
     })
+
+    test("2. Get all user data in a list",() => {
+        const user = UserService.create(1,"paralink","Marco")
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe(1)
+        expect(userInfoList[1]).toBe("paralink")
+        expect(userInfoList[2]).toBe("Marco")
+        expect(userInfoList[3]).toBe("Sin bio")
+    })
+
+    test("3. Update username", () => {
+        const user = UserService.create(1, "paralink", "Marco")
+        UserService.updateUserUsername(user, "paramar")
+        expect(user.username).toBe("paramar")
+      })
+    
+      test("4. Given a list of users give me the list of usernames", () => {
+        const user1 = UserService.create(1, "paralink1", "Marco")
+        const user2 = UserService.create(1, "paralink2", "Marco")
+        const user3 = UserService.create(1, "paralink3", "Marco")
+        const usernames = UserService.getAllUsernames([user1, user2, user3])
+        expect(usernames).toContain("paralink1")
+        expect(usernames).toContain("paralink2")
+        expect(usernames).toContain("paralink3")
+      })
 })
