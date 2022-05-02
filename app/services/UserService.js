fdf545c6f26c8514aed7edb4361ad68cde8eb4f8
mibarra24 <79391139+mibarra24@users.noreply.github.com>
2022-05-01 23:52:16 -0700

    UserSerrvice updated

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,29 +1,12 @@
-const User = require('./../models/User')
+const User = require('./../models/user')
 
 class UserService {
-    static create(id, username, name){
-        return new User(id, username, name, "Sin bio")
-    }
-
-    static getInfo(user){
-        return Object.values(user)
-    }
-
-    static getInfo(name, username){
-        user.setBio = bio
-    }
-
-   static updateBio(user, bio){
-    user.setBio = bio
+  static create(id, username, name){
+    return new User(id, username, name, "Sin bio")
   }
 
-  static updateUserUsername(user, username){
-    user.setUsername = username
-  }
-
-  static getAllUsernames(users){
-    const usersUsernames = users.map( user => user.username)
-    return usersUsernames
+  static getInfo(user){
+    return Object.values(user)
   }
 
   static updateBio(user, bio){
@@ -39,7 +22,6 @@ class UserService {
     return usersUsernames
   }
 
-
 }
 
-module.exports = UserService
+module.exports = UserServicenpm
